30f5b168fde520a60854429627a847e65ff39ba6
gouzmi <guillaume.dupont.rm@gmail.com>
2021-05-06 16:16:44 +0200

    load JSON file

--- csv_to_influxdb.py
+++ csv_to_influxdb.py
@@ -4,6 +4,7 @@ import argparse
 import csv
 import datetime
 from pytz import timezone
+import json
 
 from influxdb import InfluxDBClient
 
@@ -52,6 +53,131 @@ def isinteger(value):
         return False
 
 
+def loadJson(inputfilename, dbname, measurement, tagcolumns=None, fieldcolumns=None, servername='localhost:8086', user='root', password='root',
+            timecolumn='timestamp', timeprecision='s',
+            delimiter=",", batchsize=5000, create=False, datatimezone='UTC', usessl=False):
+    
+    """Load JSON file into InfluxDB. JSON file should be like :
+    {
+    "time_format": "%Y-%m-%d %H:%M:%S",
+    "measurement_name": "weather",
+    "weather": [
+            {
+                "datetimeEpoch": 1619647200,
+                "temp": 13.0,
+                "location": "toulouse"
+            },
+            {
+                "datetimeEpoch": 1619650800,
+                "temp": 15.0
+                "location": "toulouse"
+            }
+        ]
+    }
+
+    Args:
+        inputfilename ([type]): [description]
+        dbname ([type]): [description]
+        measurement ([type]): [description]
+        tagcolumns ([type], optional): [description]. Defaults to None.
+        fieldcolumns ([type], optional): [description]. Defaults to None.
+        servername (str, optional): [description]. Defaults to 'localhost:8086'.
+        user (str, optional): [description]. Defaults to 'root'.
+        password (str, optional): [description]. Defaults to 'root'.
+        timecolumn (str, optional): [description]. Defaults to 'timestamp'.
+        timeprecision (str, optional): [description]. Defaults to 's'.
+        delimiter (str, optional): [description]. Defaults to ",".
+        batchsize (int, optional): [description]. Defaults to 5000.
+        create (bool, optional): [description]. Defaults to False.
+        datatimezone (str, optional): [description]. Defaults to 'UTC'.
+        usessl (bool, optional): [description]. Defaults to False.
+
+    Returns:
+        [type]: [description]
+    """
+
+    host = servername[0:servername.rfind(':')]
+    port = int(servername[servername.rfind(':')+1:])
+    client = InfluxDBClient(host, port, user, password, dbname, ssl=usessl)
+
+    if(create):
+        print('Creating database %s' % dbname)
+        client.create_database(dbname)
+
+    client.switch_user(user, password)
+
+    # command line or API
+    if tagcolumns and isinstance(tagcolumns, str):
+        tagcolumns = tagcolumns.split(',')
+    if fieldcolumns and isinstance(fieldcolumns, str):
+        fieldcolumns = fieldcolumns.split(',')    
+
+    # open JSON
+    datapoints = []
+    count = 0
+    with open(inputfilename) as json_file:
+        data = json.load(json_file)
+
+    for row in data[measurement]:
+        timestamp = row[timecolumn]
+
+        tags = {}
+        if tagcolumns != None:
+
+            for t in tagcolumns:
+                v = 0
+                if t in row:
+                    v = row[t]
+                tags[t] = v
+
+        fields = {}
+        if fieldcolumns != None:
+            for f in fieldcolumns:
+                v = 0
+                if f in row:
+                    if (isfloat(row[f])):
+                        v = float(row[f])
+                    elif (isbool(row[f])):
+                        v = str2bool(row[f])
+                    else:
+                        v = row[f]
+                fields[f] = v
+
+        point = {"measurement": measurement, "time": timestamp,
+                    "fields": fields, "tags": tags}
+
+        datapoints.append(point)
+        count += 1
+
+        if len(datapoints) % batchsize == 0:
+            print('Read %d lines' % count)
+            print('Inserting %d datapoints...' % (len(datapoints)))
+            response = client.write_points(datapoints, time_precision=timeprecision)
+
+            if not response:
+                print('Problem inserting points, exiting...')
+                exit(1)
+
+            print("Wrote %d points, up to %s, response: %s" %
+                    (len(datapoints), datetime_local, response))
+
+            datapoints = []        
+
+    # write rest
+    if len(datapoints) > 0:
+        print('Read %d lines' % count)
+        print('Inserting %d datapoints...' % (len(datapoints)))
+        response = client.write_points(datapoints, time_precision=timeprecision)
+
+        if response == False:
+            print('Problem inserting points, exiting...')
+            exit(1)
+
+        print("Wrote %d, response: %s" % (len(datapoints), response))
+
+    print('Done')
+
+
 def loadCsv(inputfilename, dbname, measurement, tagcolumns=None, fieldcolumns=None, servername='localhost:8086', user='root', password='root',
             timecolumn='timestamp', timeformat='%Y-%m-%d',
             delimiter=",", batchsize=5000, create=False, datatimezone='UTC', usessl=False):
@@ -95,7 +221,6 @@ def loadCsv(inputfilename, dbname, measurement, tagcolumns=None, fieldcolumns=No
             if tagcolumns != None:
 
                 for t in tagcolumns:
-                    print(tagcolumns)
                     v = 0
                     if t in row:
                         v = row[t]
@@ -182,8 +307,8 @@ if __name__ == "__main__":
     parser.add_argument('-tc', '--timecolumn', nargs='?', default='timestamp',
                         help='Timestamp column name. Default: timestamp.')
 
-    parser.add_argument('-tf', '--timeformat', nargs='?', default='%Y-%m-%d',
-                        help='Timestamp format. Default: \'%%Y-%%m-%%d\' e.g.: 1970-01-01 00:00:00')
+    parser.add_argument('-tf', '--timeformat', nargs='?', default='%Y-%m-%d %H:%M:%S',
+                        help='Timestamp format. Default: \'%%Y-%%m-%%d %%H:%%M:%%S\' e.g.: 1970-01-01 00:00:00')
 
     parser.add_argument('-tz', '--timezone', default='UTC',
                         help='Timezone of supplied data. Default: UTC')
